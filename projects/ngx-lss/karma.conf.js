fe361fd57d5796a06b365acbd7f8a74a95e989b7
Viktor Chernodub <chernodubv@gmail.com>
2022-11-03 20:26:51 +0530

    ci: add test script

--- projects/ngx-lss/karma.conf.js
+++ projects/ngx-lss/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function (config) {
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
+        failSpecWithNoExpectations: true,
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -37,7 +38,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    // https://angular.io/guide/testing#configure-cli-for-ci-testing-in-chrome
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
